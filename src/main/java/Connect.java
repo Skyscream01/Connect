7f4b1cd3e16fbef63472a58495d85eda2a3e77bd
Yauhen Bialiayeu <yauhen.bialiayeu@icx.local>
2016-04-07 18:03:40 +0300

    Connection works

--- src/main/java/Connect.java
+++ src/main/java/Connect.java
@@ -1,12 +1,20 @@
-package main.java;
-
-/**
- * Created by yauhen.bialiayeu on 07.04.2016.
- */
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
 public class Connect
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws ClassNotFoundException , SQLException
     {
-     String d;
+      CreateConnection();
+    }
+
+    public static void CreateConnection() throws ClassNotFoundException, SQLException
+    {
+        Class.forName("org.postgresql.Driver");
+        Connection connection = null;
+        connection = DriverManager.getConnection(
+                "jdbc:postgresql://192.168.126.128:5001/test_database","tester", "qwaszx@1");
+        System.out.println("You made it, take control your database now!");
+        connection.close();
     }
 }
